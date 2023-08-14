36dec92e61cedbc4c436342bb8d2ed4ccae57912
dchow <dchow@box.com>
2023-08-14 12:21:06 -0400

    rotational forces working for Roti

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,35 @@
-// in src/main.rs
  use rusty_engine::prelude::*;
 
- // Define a struct to hold custom data for your game (it can be a lot more complicated than this one!)
+const GRAVITATIONAL_CONSTANT: f32 = -9.8;
+const FLOW_CONSTANT: f32 = 0.0;
+const FRICTIONAL_FORCE: f32 = 0.1;
+const ROTI_ROTATIONAL_ACCEL: f32 = 0.2;
+
  struct GameState {
-     health: i32,
+     roti_linear_velocity: f32,
+     roti_rotational_velocity: f32,
  }
 
  fn main() {
      // Create a game
      let mut game = Game::new();
-     // Set up your game. `Game` exposes all of the methods and fields of `Engine`.
-     let sprite = game.add_sprite("player", SpritePreset::RacingCarBlue);
-     sprite.scale = 2.0;
+
+     let sprite = game.add_sprite("roti", SpritePreset::RacingBarrelRed);
+     sprite.scale = 1.0;
+
+     let velocity_param_display = game.add_text("velocity_param_display", "Velocity: 0.0      Rotation: 0.0");
+     velocity_param_display.translation = Vec2::new(250.0, 320.0);
+
      game.audio_manager.play_music(MusicPreset::Classy8Bit, 1.0);
      // Add one or more functions with logic for your game. When the game is run, the logic
      // functions will run in the order they were added.
      game.add_logic(game_logic);
      // Run the game, with an initial state
-     let initial_game_state = GameState { health: 100 };
+     let initial_game_state = GameState {
+         roti_linear_velocity: 0.0,
+         roti_rotational_velocity: 0.0
+
+     };
      game.run(initial_game_state);
  }
 
@@ -27,13 +39,21 @@
  //
  // This function will be run once each frame.
  fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
-     // The `Engine` contains all sorts of built-in goodies.
-     // Get access to the player sprite...
-     let player = engine.sprites.get_mut("player").unwrap();
-     // Rotate the player...
-     player.rotation += std::f32::consts::PI * engine.delta_f32;
-     // Damage the player if it is out of bounds...
-     if player.translation.x > 100.0 {
-         game_state.health -= 1;
-     }
+     let roti = engine.sprites.get_mut("roti").unwrap();
+
+    if engine.keyboard_state.pressed(KeyCode::Right) {
+        game_state.roti_rotational_velocity = game_state.roti_rotational_velocity + ROTI_ROTATIONAL_ACCEL
+    };
+
+    game_state.roti_rotational_velocity = game_state.roti_rotational_velocity - FRICTIONAL_FORCE;
+
+    if game_state.roti_rotational_velocity < 0.0 {
+        game_state.roti_rotational_velocity = 0.0
+    }
+
+    roti.rotation += std::f32::consts::PI * engine.delta_f32 * game_state.roti_rotational_velocity;
+
+    let velocity_param_display = engine.texts.get_mut("velocity_param_display").unwrap();
+    velocity_param_display.value = format!("Velocity: {}     Rotation: {}", game_state.roti_linear_velocity, game_state.roti_rotational_velocity);
+
  }
